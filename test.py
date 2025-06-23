2bdc6fa52a10a4b698f16e91ea37255b4043cf0f
colin-sonarsource <colin.mueller@sonarsource.com>
2025-06-23 14:18:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,13 +10,4 @@ def bad_function():
     eval("print('Hello World')")  # Use of eval (security risk)
     return
 
-def another_bad_function():
-    password = "hardcoded_password"  # Sensitive data hardcoded
-    print("This is a debug message")  # Debug message left in code
-    os.system("ls -l")  # Use of os.system instead of subprocess (security risk)
-    x = 1 / 0  # Division by zero
-    eval("print('Hello World')")  # Use of eval (security risk)
-    return
-
 bad_function()
-another_bad_function()
